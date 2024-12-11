7960bfbec65b9a8dd611e2d262babe42b6df085f
Nahuel Gallucci <galluccinahuel@gmail.com>
2024-12-11 19:27:28 -0300

    clase 142

---
+++ Source/Aura/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
@@ -0,0 +1,22 @@
+
+#include "AbilitySystem/ExecCalc/ExecCalc_Damage.h"
+
+#include "AbilitySystemComponent.h"
+
+UExecCalc_Damage::UExecCalc_Damage()
+{
+	
+}
+
+void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams,
+	FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
+{
+	const UAbilitySystemComponent* SourceASC = ExecutionParams.GetSourceAbilitySystemComponent();
+	const UAbilitySystemComponent* TargetASC = ExecutionParams.GetTargetAbilitySystemComponent();
+
+	const AActor* SourceAvatar = SourceASC->GetAvatarActor();
+	const AActor* TargetAvatar = TargetASC->GetAvatarActor();
+
+	const FGameplayEffectSpec& Spec = ExecutionParams.GetOwningSpec();
+	
+}
